85271a4a86c05907c531a3f62e8562ca3a3d416c
Philwillen <aetheralicia@gmail.com>
2024-05-04 14:47:29 +0700

    Lê Hoàng Phi Portfolio

---
+++ src/components/about/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./about.css";
+import Image from "../../assets/avatar-2.svg";
+import AboutBox from "./AboutBox";
+
+const About = () => {
+  return (
+    <section className="about container selection" id="about">
+      <h2 className="section__title">About Me</h2>
+      <div className="about__container grid">
+        <img src={Image} alt="" className="about__img" />
+
+        <div className="about__data grid">
+          <div className="about__info">
+            <p className="about_description">
+              Tôi tên là Lê Hoàng Phi, một lập trình viên chuyên mảng frontend
+              và backend, tôi mong muốn tìm được một công việc phù hợp với sở
+              trường của mình
+            </p>
+            <a href="" className="btn">
+              Download CV
+            </a>
+          </div>
+
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Development</h3>
+                <span className="skills__number">80%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage development"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">UI/UX Design</h3>
+                <span className="skills__number">80%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage ui_design"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Photography</h3>
+                <span className="skills__number">60%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage photography"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <AboutBox />
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+      <a href="" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-instagram"></i>
+      </a>
+
+      <a href="" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-twitter"></i>
+      </a>
+
+      <a href="" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-github"></i>
+      </a>
+
+      <a href="" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-discord"></i>
+      </a>
+
+      <a href="" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-facebook"></i>
+      </a>
+      <a href="" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-youtube"></i>
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
